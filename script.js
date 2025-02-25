3caadd76009b02787c31bfdfb727511af115a517
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2025-02-25 05:15:03 +0200

    Add ypefthini

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const data = {
   xronosPeratosis: 10,
   eisaggeleiaProtodikon: "Ροδόπης",
   dieuthynsiYpiresias: "Λ Δημοκρατίας αρ. 3",
+  tilefono: "25314 40177",
+  email: "ta.komotinis@astynomia.gr",
 };
 
 // ΕΚΘΕΣΗ ΜΑΡΤΥΡΑ ΧΩΡΙΣ
@@ -208,6 +210,240 @@ function printGnostopoiisi(initial, person, datas, date, timeFormatter) {
 <p>Για πιστοποίηση συντάχθηκε η παρούσα έκθεση, η οποία αφού αναγνώσθηκε και βεβαιώθηκε, υπογράφεται ως ακολούθως:</p>
 `;
 }
+function printYpiresiako(
+  axyp,
+  person,
+  datas,
+  day,
+  month,
+  year,
+  time,
+  protokolo,
+  apolesthen
+) {
+  return `<div class="container" style="width: 210mm; height: 297mm; margin: 20px auto; display: flex; flex-direction: column;">
+		<div style="display: flex; flex-direction: column; align-items: center; align-self:flex-start">
+		  <p style="margin:0;">
+			<b>ΕΛΛΗΝΙΚΗ ΔΗΜΟΚΡΑΤΙΑ</b></fon>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΥΠΟΥΡΓΕΙΟ ΠΡΟΣΤΑΣΙΑΣ ΤΟΥ ΠΟΛΙΤΗ</b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΑΡΧΗΓΕΙΟ ΕΛΛΗΝΙΚΗΣ ΑΣΤΥΝΟΜΙΑΣ</b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΓΕΝΙΚΗ ΠΕΡΙΦΕΡΕΙΑΚΗ ΑΣΤΥΝΟΜΙΚΗ </b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΔΙΕΥΘΥΝΣΗ ΑΝΑΤΟΛΙΚΗΣ ΜΑΚΕΔΟΝΙΑΣ-ΘΡΑΚΗΣ</b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΔΙΕΥΘΥΝΣΗ ΑΣΤΥΝΟΜΙΑΣ ΡΟΔΟΠΗΣ</b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΤΜΗΜΑ ΔΙΩΞΗΣ ΚΑΙ ΕΞΙΧΝΙΑΣΗΣ</b>
+		  </p>
+		  <p style="margin:0;">
+			<b>ΕΓΚΛΗΜΑΤΩΝ ΚΟΜΟΤΗΝΗΣ</b>
+		  </p>
+		  <p style="margin:0;">
+			Δ/νση: ${datas.dieuthynsiYpiresias} ${datas.merosSyntaksisEkthesis}
+		  </p>
+		  <p style="margin:0;">
+			Αρμόδιος: ${axyp}.
+		  </p>
+		  <p style="margin:0;">
+			Τηλέφωνο<span lang="en-US">: ${datas.tilefono}</span>
+		  </p>
+		  <p style="margin:0;">
+			<span lang="en-US">e-mail: ${datas.email}</span>
+		  </p>
+		  <p style="margin:0;">
+			<u><b>ΑΡ. ΠΡΩΤ:3005/4/${protokolo}-α</b></u>
+		  </p>
+		</div>
+		<p align="center" style="line-height: 150%; margin-bottom: 0cm"><br /></p>
+		<p align="center" style="line-height: 150%; margin-bottom: 0cm">
+		  <font size="3" style="font-size: 13pt"
+			><u><b>ΥΠΗΡΕΣΙΑΚΟ ΣΗΜΕΙΩΜΑ</b></u></font
+		  >
+		</p>
+		<p align="center" style="line-height: 150%; margin-bottom: 0cm"><br /></p>
+		<p align="justify" style="line-height: 115%; margin-bottom: 0cm">
+		  Αναφέρεται ότι την ${day} ${month} ${year} και ώρα ${time} προσήλθε στην Υπηρεσία μας ο ${person} και με υπεύθυνη δήλωση του Ν1599/1986 δήλωσε ότι απώλεσε σε άγνωστο τόπο και χρόνο ${apolesthen}</p>
+		<div align="right">
+		  <table width="345" cellpadding="7" cellspacing="0">
+			<col width="331" />
+			<tr>
+			  <td
+				width="331"
+				height="255"
+				valign="top"
+				style="border: none; padding: 0cm"
+			  >
+				<p align="center" style="margin-bottom: 0cm">
+				  <br />
+				</p>
+				<p align="center" style="margin-bottom: 0cm">
+				  <font size="3" style="font-size: 13pt"
+					>${datas.merosSyntaksisEkthesis}, ${day} ${month} ${year}</font
+				  >
+				</p>
+				<p align="center" style="margin-bottom: 0cm"><br /></p>
+				<p align="center" style="margin-bottom: 0cm">
+				  <font size="3" style="font-size: 13pt"><b>Ο</b></font>
+				</p>
+				<p align="center" style="margin-bottom: 0cm">
+				  <font size="3" style="font-size: 13pt"
+					><b>Αξιωματικός Υπηρεσίας</b></font
+				  >
+				</p>
+				<p style="margin-bottom: 0cm"><br /></p>
+				<p align="center" style="margin-bottom: 0cm"><br /></p>
+				<p align="center" style="margin-bottom: 0cm">
+				  <font size="3" style="font-size: 13pt"
+					><b>${axyp.split(" ")[1]}Σ ${axyp.split(" ")[2]}ς</b></font
+				  >
+				</p>
+				<p align="center" style="margin-bottom: 0cm">
+				  <font size="3" style="font-size: 13pt"><b>${axyp.split(" ")[0]}</b></font>
+				</p>
+				<p><br /></p>
+			  </td>
+			</tr>
+		  </table>
+		</div>
+		<p style="line-height: 100%; margin-bottom: 0cm"></p>
+	</div>`;
+}
+function printYpefthini(
+  datas,
+  axyp,
+  person,
+  protokolo,
+  apolesthen,
+  date,
+  dateFormatter
+) {
+  return `<div
+  style="
+    font-family: Arial, sans-serif;
+    width: 210mm;
+    height: 297mm;
+    margin: 20px auto;
+    line-height: 1.5;
+  "
+>
+  <div style="text-align: center; margin-bottom: 20px">
+    <h3 style="margin-bottom: 10px">ΥΠΕΥΘΥΝΗ ΔΗΛΩΣΗ</h3>
+    <h3 style="margin-top: 5px"><sup>(άρθρο 8 Ν.1599/1986)</sup></h3>
+  </div>
+  <div style="font-size: 0.8em; text-align: end">${axyp}</div>
+  <div style="font-size: 0.8em; text-align: end">Χορηγήθηκε Υ.Σ.</div>
+  <div style="font-size: 0.8em; text-align: end">3005/4/${protokolo}</div>
+
+  <p style="margin-bottom: 20px; font-style: italic">
+    Η ακρίβεια των στοιχείων που υποβάλλονται με αυτή τη δήλωση μπορεί να
+    ελεγχθεί με βάση το αρχείο άλλων υπηρεσιών (άρθρο 8 παρ. 4 Ν. 1599/1986)
+  </p>
+
+  <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px">
+    <tr>
+      <td style="width: 20%; padding: 5px; border: 1px solid #000">ΠΡΟΣ:</td>
+      <td style="width: 80%; padding: 5px; border: 1px solid #000" colspan="5">
+        <strong>${datas.ypiresia}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">Ο – Η Όνομα:</td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2">
+        <strong>${person.firstName}</strong>
+      </td>
+      <td style="padding: 5px; border: 1px solid #000; width: 15%">ΕΠΩΝΥΜΟ:</td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2">
+        <strong>${person.surname}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">
+        Όνομα και Επώνυμο Πατέρα:
+      </td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="5">
+        <strong>${person.fatherName} ${person.surname}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">
+        Όνομα και Επώνυμο Μητέρας:
+      </td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="5">
+        <strong>${person.motherName} ${person.motherSurname}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">Ημερομηνία γέννησης:</td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="5">
+        <strong>${person.birthDate}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">Τόπος Γέννησης:</td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="5">
+        <strong>${person.birthPlace}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">
+        Αριθμός Δελτίου Ταυτότητας:
+      </td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2">
+        <strong>${person.idNumber}</strong>
+      </td>
+      <td style="padding: 5px; border: 1px solid #000">Τηλ:</td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2">
+        <strong>${person.phoneNumber}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000">Τόπος Κατοικίας:</td>
+      <td style="padding: 5px; border: 1px solid #000">
+        <strong>${person.area}</strong>
+      </td>
+      <td style="padding: 5px; border: 1px solid #000; width: 10%">Οδός:</td>
+      <td style="padding: 5px; border: 1px solid #000">${person.street}</td>
+      <td style="padding: 5px; border: 1px solid #000; width: 10%">Αριθ:</td>
+      <td style="padding: 5px; border: 1px solid #000">${
+        person.streetNumber
+      }</td>
+    </tr>
+    <tr>
+      <td style="padding: 5px; border: 1px solid #000; width: 10%">ΤΚ:</td>
+      <td style="padding: 5px; border: 1px solid #000"></td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2">
+        Δ/νση Ηλεκτρ. Ταχυδρομείου (Email):
+      </td>
+      <td style="padding: 5px; border: 1px solid #000" colspan="2"></td>
+    </tr>
+  </table>
+
+  <div style="border: 1px solid #000; padding: 15px; margin-bottom: 30px">
+    <p style="font-weight: bold">
+      Με ατομική μου ευθύνη και γνωρίζοντας τις κυρώσεις, που προβλέπονται από
+      τις διατάξεις της παρ. 6 του άρθρου 22 του Ν. 1599/1986, δηλώνω ότι:\n
+    Σε άγνωστο τόπο και χρόνο απώλεσα ${apolesthen}</p>
+  </div>
+
+  <div style="text-align: right; margin-top: 30px; margin-right: 50px">
+    <p><strong>${datas.merosSyntaksisEkthesis}, ${dateFormatter(
+    date
+  )}</strong></p>
+    <p style="margin-top: 30px">Ο-Η Δηλών-ούσα</p>
+    <p style="margin-top: 40px">.</p>
+  </div>
+</div>
+`;
+}
 
 //Μην πειράξετε από εδώ και κάτω
 
@@ -247,6 +483,7 @@ let formattedTime = formatTime(today, 0);
 let year = today.getFullYear();
 let month = months[today.getMonth()];
 let day = today.getDate();
+let ypefthiniData = {};
 
 const anakritikosSelect = document.querySelector("#anakritikos");
 const bAnakritikosSelect = document.querySelector("#anakritikos-b");
@@ -275,6 +512,19 @@ const initialText = document.getElementById("initial");
 function constructInitialText() {
   return `Στην ${data.merosSyntaksisEkthesis} σήμερα την ${day}η του μήνα ${month} του έτους ${year} ημέρα ${dayName} και ώρα ${formattedTime} ενώπιον εμού, του ${anakritikosSelect.value} του ${data.ypiresia}, παρισταμένου και του ${bAnakritikosSelect.value} `;
 }
+
+//dilosi apoleias
+let apolesthenItem = "";
+let protokoloNumber = 0;
+const apolesthen = document.getElementById("apolesthen");
+apolesthen.addEventListener("input", () => {
+  apolesthenItem = apolesthen.value;
+});
+
+const protokolo = document.getElementById("protokolo");
+protokolo.addEventListener("input", () => {
+  protokoloNumber = protokolo.value;
+});
 
 // Initial setup
 initialText.textContent = constructInitialText();
@@ -455,6 +705,8 @@ katigoroumenou.addEventListener("click", () => {
   );
   copyToClipboard(text);
 });
+
+//apodosi button
 const apodosi = document.getElementById("apodosi");
 apodosi.addEventListener("click", () => {
   downloadAsWord(
@@ -497,6 +749,40 @@ gnostopoiisi.addEventListener("click", () => {
       formatTime
     ),
     "gnostopoiisi"
+  );
+});
+// ypiresiako button
+const ypiresiako = document.getElementById("ypiresiako");
+ypiresiako.addEventListener("click", () => {
+  downloadAsWord(
+    printYpiresiako(
+      anakritikosSelect.value,
+      clipboardId.value,
+      data,
+      day,
+      month,
+      year,
+      formattedTime,
+      protokoloNumber,
+      apolesthenItem
+    ),
+    "ypiresiako"
+  );
+});
+// ypefthini button
+const ypefthini = document.getElementById("ypefthini");
+ypefthini.addEventListener("click", () => {
+  downloadAsWord(
+    printYpefthini(
+      data,
+      anakritikosSelect.value,
+      ypefthiniData,
+      protokoloNumber,
+      apolesthenItem,
+      today,
+      formatDate
+    ),
+    "ypefthini"
   );
 });
 function downloadAsWord(func, id) {
@@ -659,6 +945,7 @@ function formatIdInfo(input, data) {
     firstName: getValue("Όνομα"),
     fatherName: getValue("Όνομα Πατρός"),
     motherName: getValue("Όνομα Μητρός"),
+    motherSurname: getValue("Επώνυμο Μητρός"),
     birthDate: formatDate(getValue("Ημ/νία Γέννησης")),
     birthPlace: getValue("Τόπος Γέννησης").split(" ")[0],
     area: getValue("Περιοχή"),
@@ -687,6 +974,9 @@ function formatIdInfo(input, data) {
   if (fields.region === "Περιοχή") {
     fields.region = "--- ";
   }
+  // extract the data for ypefthini dilosi usage
+  ypefthiniData = fields;
+  console.log(ypefthiniData);
 
   // Format the output string
   const formattedString = `${fields.surname} ${capitalize(
