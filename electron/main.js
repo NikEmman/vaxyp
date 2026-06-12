ba337261c818294470d3721ca707e1c13cf6d214
NikEmman <121699424+NikEmman@users.noreply.github.com>
2026-06-12 22:45:54 +0300

    Electron app has spell check, right click copy/paste

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, protocol, net } = require("electron");
+const { app, BrowserWindow, protocol, net, Menu, MenuItem, session } = require("electron");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
@@ -16,12 +16,12 @@ protocol.registerSchemesAsPrivileged([
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 900,
+    show: false,
     icon: path.join(__dirname, "../images/logo.png"),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      spellcheck: true,
     },
   });
 
@@ -31,6 +31,74 @@ function createWindow() {
   } else {
     win.webContents.openDevTools();
   }
+
+  win.maximize();
+  win.show();
+
+  win.webContents.session.setSpellCheckerLanguages(["el-GR", "en-US"]);
+
+  win.webContents.on("context-menu", (event, params) => {
+    const menu = new Menu();
+
+    for (const suggestion of params.dictionarySuggestions) {
+      menu.append(
+        new MenuItem({
+          label: suggestion,
+          click: () => win.webContents.replaceMisspelling(suggestion),
+        })
+      );
+    }
+
+    if (params.misspelledWord) {
+      if (params.dictionarySuggestions.length > 0) {
+        menu.append(new MenuItem({ type: "separator" }));
+      }
+      menu.append(
+        new MenuItem({
+          label: "Προσθήκη στο λεξικό",
+          click: () =>
+            session.defaultSession.addWordToSpellCheckerDictionary(
+              params.misspelledWord
+            ),
+        })
+      );
+      menu.append(new MenuItem({ type: "separator" }));
+    }
+
+    if (params.isEditable) {
+      menu.append(
+        new MenuItem({
+          label: "Αποκοπή",
+          role: "cut",
+          enabled: params.editFlags.canCut,
+        })
+      );
+    }
+
+    if (params.isEditable || params.selectionText) {
+      menu.append(
+        new MenuItem({
+          label: "Αντιγραφή",
+          role: "copy",
+          enabled: params.editFlags.canCopy,
+        })
+      );
+    }
+
+    if (params.isEditable) {
+      menu.append(
+        new MenuItem({
+          label: "Επικόλληση",
+          role: "paste",
+          enabled: params.editFlags.canPaste,
+        })
+      );
+    }
+
+    if (menu.items.length > 0) {
+      menu.popup({ window: win });
+    }
+  });
 
   win.loadURL("app://./index.html");
 }
